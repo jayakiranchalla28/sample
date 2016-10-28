af7f75ae1035ac23b9238153ef081ab6d5c7be71
girees <macbookpro@iMac.local>
2016-10-28 10:50:12 +0530

    Initial Commit

---
+++ hfh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hfh
+//
+//  Created by Wifin Tech on 10/28/16.
+//  Copyright © 2016 wifin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hfh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hfh
+//
+//  Created by Wifin Tech on 10/28/16.
+//  Copyright © 2016 wifin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
